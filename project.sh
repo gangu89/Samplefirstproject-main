1bfec13f606aeb1e41aa652292781a56a9f6244b
gangu89 <44300717+gangu89@users.noreply.github.com>
2025-07-18 23:12:51 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,1 +1,1 @@
-echo "Gangadhara This is my Jenkins fullcourse Tutorial. This is my Third Testing complate" 
+echo " testGangadhara This is my Jenkins fullcourse Tutorial. This is my Third Testing complate" 
